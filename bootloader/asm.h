a0e108a729c7db86420ff7fde911d2a1d5ceb582
MichaelCache <kuangzhan4ba@163.com>
2015-12-11 11:34:47 +0800

    Version 0.2

--- bootloader/asm.h
+++ bootloader/asm.h
@@ -9,9 +9,9 @@
     .byte 0, 0, 0, 0
 
 #define SEG_ASM(type,base,lim)                                  \
-    .word (((lim) >> 12) & 0xffff), ((base) & 0xffff);          \
+    .word (((lim) & 0xffff), ((base) & 0xffff);          \
     .byte (((base) >> 16) & 0xff), (0x90 | (type)),             \
-        (0xC0 | (((lim) >> 28) & 0xf)), (((base) >> 24) & 0xff)
+        ( (((lim) >> 28) & 0xf)), 0xc0,(((base) >> 24) & 0xff)
 
 
 /* Application segment type bits */
